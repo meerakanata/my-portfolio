7854e1fa32eefe29d98acaa6b1181c0e9bae1a2b
Meera Shah <meera_kanata@yahoo.ca>
2022-09-05 15:01:19 -0400

    updated resume download link

--- src/Pages/Components/ContactBar.js
+++ src/Pages/Components/ContactBar.js
@@ -10,7 +10,7 @@ function ContactBar() {
             <h4><a href="mailto:meera31kanata@gmail.com" className="contactBar__email">meera31kanata@gmail.com</a></h4>
             <EmailIcon className={"contactBar__emailMobile"}/>
             <a id="LinkedInIcon" href="https://www.linkedin.com/in/meera-uxui/" target="_blank"><LinkedInLogo alt="LinkedIn logo" className="contactBar__child"/></a>
-            <a href="./Resume/Resume/MeeraShah-Resume.pdf" download="MeeraShah-Resume">
+            <a href="./Resume/MeeraShah-Resume.pdf" download="MeeraShah-Resume">
                 <DownloadButton className="contactBar__dnldButton"/>
             </a>
         </div>
